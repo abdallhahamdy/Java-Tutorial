e9d2f39ee770643728db6e863c206af199778291
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-04 17:35:32 +0300

    ArrayToArrayList for Integer

--- Array/Tutorial/java.util.Arrays Class API Guide/ArrayToArrayList.java
+++ Array/Tutorial/java.util.Arrays Class API Guide/ArrayToArrayList.java
@@ -4,22 +4,26 @@ import java.util.List;
 public class ArrayToArrayList {
 
     public static void main(String[] args) {
-        String anArrayOfStrings[] = { "Agra", "Mysore", "Chandigarh", "Bhopal"};
+//        String anArrayOfStrings[] = { "Agra", "Mysore", "Chandigarh", "Bhopal"};
+//
+//        List<String> strList = Arrays.asList(anArrayOfStrings);
 
-        List<String> strList = Arrays.asList(anArrayOfStrings);
+        Integer anArrayOfIntegers[] = { 1, 2, 3, 4, 5, 6};
+
+        List<Integer> intList = Arrays.asList(anArrayOfIntegers);
 
         System.out.println("Original ArrayList from Arrays.asList()");
 
         /* Display array list */
-        strList.forEach(str -> System.out.println(" " + str));
+        intList.forEach(str -> System.out.println(" " + str));
 
         // change the array element and see the effect is propogated to list
         // also.
-        anArrayOfStrings[0] = "Dehli";
+        anArrayOfIntegers[0] = -1;
 
         System.out.println("\nChange in array effect on ArrayList");
 
         /* Display array list */
-        strList.forEach(str -> System.out.println(" " + str));
+        intList.forEach(str -> System.out.println(" " + str));
     }
 }
