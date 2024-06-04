8deb701b08142a8069a195b87b106ba3c77e4fbb
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-04 19:55:41 +0300

    Sorting An Array

---
+++ Array/Tutorial/java.util.Arrays Class API Guide/SortingAnArray.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class SortingAnArray {
+
+    public static void main(String[] args) {
+        // Sorts the specified array into ascending numerical order.
+        int[] intArray = { 3, 1, 2, 4 };
+        System.out.println("Original Array : " + Arrays.toString(intArray));
+        Arrays.sort(intArray);
+        System.out.println("Sorted Array : " + Arrays.toString(intArray));
+
+        // Sorts the specified array into ascending numerical order.
+        byte[] byteArray = { 1, 3, 2, 1, 5 };
+        System.out.println("Original Array : " + Arrays.toString(byteArray));
+        Arrays.sort(byteArray);
+        System.out.println("Sorted Array : " + Arrays.toString(byteArray));
+
+        // Sorts the specified array into ascending numerical order.
+        char[] charArray = { 'a', 'd', 'c','b' };
+        System.out.println("Original Array : " + Arrays.toString(charArray));
+        Arrays.sort(charArray);
+        System.out.println("Sorted Array : " + Arrays.toString(charArray));
+
+        // Sorts the specified array into ascending numerical order.
+        double[] doubleArray = { 0.1, 0.3, 0.2 };
+        System.out.println("Original Array : " + Arrays.toString(doubleArray));
+        Arrays.sort(doubleArray);
+        System.out.println("Sorted Array : " + Arrays.toString(doubleArray));
+
+        // Sorts the specified array into ascending numerical order.
+        long[] longArray = { 1, 3, 2, 5, 4 };
+        System.out.println("Original Array : " + Arrays.toString(longArray));
+        Arrays.sort(longArray);
+        System.out.println("Sorted Array : " + Arrays.toString(longArray));
+
+        // Sorts the specified array into ascending numerical order.
+        float[] floatArray = { 1.1f, 1.5f, 1.4f };
+        System.out.println("Original Array : " + Arrays.toString(floatArray));
+        Arrays.sort(floatArray);
+        System.out.println("Sorted Array : " + Arrays.toString(floatArray));
+    }
+
+}
