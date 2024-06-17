680473e7e4a9f55ce481607f8d714af2614d50cf
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-17 10:45:38 +0300

    Queue Implementation using Linked List

---
+++ Data Structure & Algorithm/Queue/Queue Implementation using Linked List/ListNode.java
@@ -0,0 +1,46 @@
+/**
+ * Class demonstrates the declaration for a linked list.
+ * @author Abdallha
+ *
+ */
+public class ListNode {
+    public ListNode next;
+    public int data;
+
+    // Creates an empty node.
+    public ListNode() {
+        next = null;
+        data = Integer.MIN_VALUE;
+    }
+
+    // Creates a node storing the specified data.
+    public ListNode (int data) {
+        next = null;
+        this.data = data;
+    }
+
+    // Returns the node follows this one.
+    public ListNode getNext() {
+        return next;
+    }
+
+    // Sets the node that follows this one.
+    public void setNext (ListNode node) {
+        next = node;
+    }
+
+    // Returns the data stored in this node.
+    public int getData() {
+        return data;
+    }
+
+    // Sets the data stored in this node.
+    public void setData (int elem) {
+        data = elem;
+    }
+
+    // Sets the data stored in this node.
+    public String toString() {
+        return Integer.toString(data);
+    }
+}
