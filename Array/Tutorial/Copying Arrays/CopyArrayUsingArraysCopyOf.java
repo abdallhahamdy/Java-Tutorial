1319c69c6c1f4101cb5a3a4b215654b52e37a93c
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-19 18:35:54 +0300

    Moving Folders Array

--- Array/Tutorial/Copying Arrays/CopyArrayUsingArraysCopyOf.java
+++
@@ -1,9 +0,0 @@
-public class CopyArrayUsingArraysCopyOf {
-    public static void main(String[] args) {
-        int[] original = {1, 2, 3, 4, 5};
-        int[] copy = java.util.Arrays.copyOf(original, original.length);
-
-        System.out.println("Original array: " + java.util.Arrays.toString(original));
-        System.out.println("Copied array: " + java.util.Arrays.toString(copy));
-    }
-}
--- Array/Tutorial/Copying Arrays/CopyArrayUsingClone.java
+++
@@ -1,9 +0,0 @@
-public class CopyArrayUsingClone {
-    public static void main(String[] args) {
-        int[] original = {1, 2, 3, 4, 5};
-        int[] copy = original.clone();
-
-        System.out.println("Original array: " + java.util.Arrays.toString(original));
-        System.out.println("Copied array: " + java.util.Arrays.toString(copy));
-    }
-}
