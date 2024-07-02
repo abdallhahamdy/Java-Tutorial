82353d07f11ddcf38a2f72a1f1351f88c0945b6f
abdallhahamdy <bbdallhhmdy@gmail.com>
2024-07-02 06:34:32 +0300

    Cohesion OOP Example_3

---
+++ OOP/Cohesion/Example_3/Order.java
@@ -0,0 +1,19 @@
+package Cohesion.Example_3;
+
+import java.util.List;
+
+public class Order {
+    private List<Product> products;
+
+    public Order(List<Product> products) {
+        this.products = products;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
