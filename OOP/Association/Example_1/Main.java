13feb85fcd35802af3835cd875f15059259fb162
abdallhahamdy <bbdallhhmdy@gmail.com>
2024-07-02 03:14:21 +0300

    Association OOP Example_1

---
+++ OOP/Association/Example_1/Main.java
@@ -0,0 +1,17 @@
+package Association.Example_1;
+
+public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book("1984", "George Orwell");
+        Book book2 = new Book("To Kill a Mockingbird", "Harper Lee");
+
+        Library library = new Library();
+        library.addBook(book1);
+        library.addBook(book2);
+
+        library.showBooks();
+        // Output:
+        // Title: 1984, Author: George Orwell
+        // Title: To Kill a Mockingbird, Author: Harper Lee
+    }
+}
