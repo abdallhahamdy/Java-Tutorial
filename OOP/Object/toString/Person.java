7c70aa7814752831217f9e8b852c27e5b5ca0568
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-07-06 15:43:38 +0300

    Object OOP toString

---
+++ OOP/Object/toString/Person.java
@@ -0,0 +1,36 @@
+package Object.toString;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Person [firstName=" + firstName + ", lastName=" + lastName + "]";
+    }
+
+    public static void main(String[] args)  {
+        Person person = new Person();
+        person.setFirstName("Ramesh");
+        person.setLastName("Fadatare");
+        System.out.println(person.toString());
+
+    }
+}
