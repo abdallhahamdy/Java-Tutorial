0997a34b62ef564467f9c86881136d27e7cfb5db
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-07-06 11:21:22 +0300

    Object OOP Class<?> getClass()

---
+++ OOP/Object/ClassGetClass/Person.java
@@ -0,0 +1,27 @@
+package Object.ClassGetClass;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public static void main(String[] args)  {
+        Person person = new Person();
+        System.out.println(person.getClass());
+    }
+}
