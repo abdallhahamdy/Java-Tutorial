86352ddfba591ad42cd7a67b59fef06829315eb4
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-08-05 16:42:34 +0300

    How to Create Thread using Lambda Expressions in Java? Example_2

---
+++ Java 8/Lambda/LambdaThread/Example_2.java
@@ -0,0 +1,22 @@
+package LambdaThread;
+
+public class Example_2 {
+
+    public static void main(String[] args) {
+        Runnable basic = () ->
+        {
+            String threadName
+                    = Thread.currentThread().getName();
+            System.out.println("Running common task by "
+                                + threadName);
+        };
+
+        // Instantiating two thread classes
+        Thread thread1 = new Thread(basic);
+        Thread thread2 = new Thread(basic);
+
+        // Running two threads for the same task
+        thread1.start();
+        thread2.start();
+    }
+}
