b9485935d275b2d2b23c7c1a63f1dc1146e2925b
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-08-08 13:09:06 +0300

    LinkedList AddLast

--- Collections/LinkedList/LinkedListExample.java
+++ Collections/LinkedList/LinkedListExample.java
@@ -4,7 +4,7 @@ public class LinkedListExample {
     public static void main(String[] args) {
         LinkedList<String> fruits = new LinkedList();
         fruits.add("Apple");
-        fruits.add("Banana");
+        fruits.addLast("Banana22");
         fruits.add("Mango");
         fruits.addFirst("Watermalon");
         System.out.println(fruits);
