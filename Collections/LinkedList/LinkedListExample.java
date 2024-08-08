36cd0af7d14bd0968b18c274332352b9268d85de
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-08-08 13:18:18 +0300

    LinkedList AddAll

--- Collections/LinkedList/LinkedListExample.java
+++ Collections/LinkedList/LinkedListExample.java
@@ -1,12 +1,11 @@
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class LinkedListExample {
     public static void main(String[] args) {
         LinkedList<String> fruits = new LinkedList();
         fruits.add("Apple");
-        fruits.addLast("Banana22");
-        fruits.add("Mango");
-        fruits.addFirst("Watermalon");
+        fruits.addAll(Arrays.asList("Banana", "Mango"));
         System.out.println(fruits);
     }
 }
