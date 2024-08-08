4ef66502ee7c754ad6bade4b7c282f16733c758c
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-08-08 13:44:54 +0300

    LinkedList remove

--- Collections/LinkedList/LinkedListExample.java
+++ Collections/LinkedList/LinkedListExample.java
@@ -5,8 +5,13 @@ public class LinkedListExample {
     public static void main(String[] args) {
         LinkedList<String> fruits = new LinkedList();
         fruits.add("Apple");
-        fruits.addAll(Arrays.asList("Banana", "Mango"));
-        fruits.set(1, "Orange");
+        fruits.add("Banana");
+        fruits.add("Mango");
+        fruits.remove(1);
+
+        System.out.println(fruits);
+
+        fruits.remove("Mango");
         System.out.println(fruits);
     }
 }
