11ffd7b17d6abb7940d45be18bc572bc4297af0c
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-08-08 13:05:47 +0300

    LinkedList Add

---
+++ Collections/LinkedList/LinkedListExample.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+
+public class LinkedListExample {
+    public static void main(String[] args) {
+        LinkedList<String> fruits = new LinkedList();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+        System.out.println(fruits);
+    }
+}
