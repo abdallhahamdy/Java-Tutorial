ac2aea948c9ed3d11a708d8208895ff962b1f6cc
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-08-18 15:11:12 +0300

    HashMap Adding_Element

---
+++ Collections/HashMap/Adding_Element.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class Adding_Element {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("Apple", 10);
+        map.put("Orange", 20);
+        map.put("Banana", 30);
+
+        System.out.println(map);
+    }
+}
